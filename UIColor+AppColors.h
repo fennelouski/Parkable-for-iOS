2c65c1e6e871e76748bcc51c6fefc64e07cbeaac
Nathan Fennel <nathanfennel@gmail.com>
2015-10-21 15:20:22 -0400

    Added App Color

---
+++ UIColor+AppColors.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+AppColors.h
+//  Parkable
+//
+//  Created by HAI on 10/21/15.
+//  Copyright © 2015 Nathan Fennel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (AppColors)
+
++ (UIColor *)appColor;
+
+@end
---
+++ UIColor+AppColors.m
@@ -0,0 +1,17 @@
+//
+//  UIColor+AppColors.m
+//  Parkable
+//
+//  Created by HAI on 10/21/15.
+//  Copyright © 2015 Nathan Fennel. All rights reserved.
+//
+
+#import "UIColor+AppColors.h"
+
+@implementation UIColor (AppColors)
+
++ (UIColor *)appColor {
+	return [UIColor colorWithRed:19.0f/255.0f green:56.0f/255.0f blue:92.0f/255.0f alpha:1.0f];
+}
+
+@end
