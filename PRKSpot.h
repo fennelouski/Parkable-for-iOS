8a53a0cf0b368f9e34167fb5498dc22ddb4a04b1
Nathan Fennel <nathanfennel@gmail.com>
2015-10-21 17:12:46 -0400

    Added PRKSpot Class

---
+++ PRKSpot.h
@@ -0,0 +1,13 @@
+//
+//  PRKSpot.h
+//  Parkable
+//
+//  Created by HAI on 10/21/15.
+//  Copyright © 2015 Nathan Fennel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PRKSpot : NSObject
+
+@end
---
+++ PRKSpot.m
@@ -0,0 +1,13 @@
+//
+//  PRKSpot.m
+//  Parkable
+//
+//  Created by HAI on 10/21/15.
+//  Copyright © 2015 Nathan Fennel. All rights reserved.
+//
+
+#import "PRKSpot.h"
+
+@implementation PRKSpot
+
+@end
